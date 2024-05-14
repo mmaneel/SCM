146d5f70fc1cf25b2bf39050d05df1af79733df6
mmaneel <m.mohandouali@esi-sba.dz>
2024-05-14 04:04:30 +0200

    test

--- back/pl_sql/api.py
+++ back/pl_sql/api.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Importez Flask-CORS
-
+from flask_cors import CORS
 import cx_Oracle
 from create_commande import commande_creation
 from cursor_execute import *
@@ -9,14 +8,11 @@ app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000"]}})
 
 # Assuming cx_Oracle setup and connection code is already done
-
 @app.route('/api/create_commande', methods=['POST'])
 def create_commande():
     data = request.json
-    
-    # Extract data from JSON
-    p_nom_client = data.get("p_prenom_client")
-    p_prenom_client = data.get("p_nom_client")
+    p_nom_client = data.get("p_nom_client")
+    p_prenom_client = data.get("p_prenom_client")
     p_num_tel = data.get("p_num_tel")
     p_numero_carte = data.get("p_numero_carte")
     p_cvv = data.get("p_cvv")
@@ -25,12 +21,10 @@ def create_commande():
     p_type_id = data.get("p_type_i")
     p_adresse = data.get("p_adresse")
     p_code_postal = data.get("p_code_postal")
-    product_quantity = data.get("product_quantity")
+    product_quantity_list = data.get("product_quantity")
 
-    commande_creation(p_nom_client,p_prenom_client,p_num_tel,p_numero_carte,p_cvv,p_date_exp,p_balance,p_type_id,p_adresse,p_code_postal,product_quantity)
-    
+    commande_creation(p_nom_client, p_prenom_client, p_num_tel, p_numero_carte, p_cvv, p_date_exp, p_balance, p_type_id, p_adresse, p_code_postal, product_quantity_list)
     return jsonify({'message': 'Order created successfully'})
-
 
 # Endpoint for retrieving products
 @app.route('/api/products', methods=['GET'])
@@ -38,13 +32,11 @@ def get_all_products():
     data = get_products()
     return jsonify(data)
 
-
 # Endpoint for retrieving orders
 @app.route('/api/orders', methods=['GET'])
 def get_orders():
     data = get_orders()
     return jsonify(data)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
